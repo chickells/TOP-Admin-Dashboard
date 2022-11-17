e3054a90debd42d2b372783749c8202760b6a0d9
Chase <cnickellsumn@gmail.com>
2022-11-17 11:16:16 -0800

    practice js stuff

--- library/script.js
+++ library/script.js
@@ -45,5 +45,71 @@ let pockets = {
     money: 2000
 };
 
-console.log(pockets.pen);
-console.log(bed.glasses);
+// console.log(pockets.pen);
+// console.log(bed.glasses);
+
+// function Dog(name, color) {
+//     this.name = name;
+//     this.color = color;
+//   }
+//   // Only change code below this line
+//   let hound = new Dog('Chaz', 'brown')
+//   console.log(hound);
+//   console.log(book1 instanceof Dog);
+
+//   Dog.prototype.numLegs = 4;
+
+//   let ownProps = []
+//   let prototypeProps = []
+
+//   for (let thing in hound) {
+//     if (hound.hasOwnProperty(thing)) {
+//         ownProps.push(thing);
+//     } else {
+//         prototypeProps.push(thing);
+//     }
+//   }
+//   console.log(ownProps);
+//   console.log(prototypeProps);
+
+  //////////////////////////////
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    // Only change code below this line
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      return "ate ur moms ass"
+    },
+    describe: function() {
+      console.log("My name is " + this.name + " and I'm here to " + this.eat())
+    },
+  };
+
+  let pooch = new Dog('Dicky')
+  pooch.describe()
+
+console.log(pooch instanceof Dog);
+console.log(Dog.prototype.isPrototypeOf(pooch));
+
+///////////////////////////////
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype) // Change this line
+duck.eat()
+console.log(duck instanceof Animal);
+let beagle; // Change this line
