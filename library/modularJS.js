a1a1790f0b7372458f16ad2e0aaac63e030be27c
Chase <cnickellsumn@gmail.com>
2022-11-23 06:15:17 -0800

    start modular JS #7 video about prototypical/classical inheritance

--- library/modularJS.js
+++ library/modularJS.js
@@ -1,3 +1,16 @@
+// 11.23.22 6:13a update
+// just finished Modular JS #6, now on to #7
+// https://www.youtube.com/watch?v=doXpW5AD60Q&list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f&index=7
+// talking about inheritance and prototypes, etc. seems to be sort of making sense?
+// basically talking about concepts of keeping certain aspects of your code
+// private within that chunk, and can't be accessed/modified outside of the desired scope
+//
+// introducing the aspect of security. alright. let's meditate and head to work!
+// THIS FUCKING COFFEE IS SO FUCKING GOOD I FINALLY FOUND THE BLUEBERRY FLAVOR!!!!!!
+
+
+////////////////////////////////////////
+
 //object literal
 var myModule = {
     name: 'Will',
@@ -21,7 +34,7 @@ myModule.sayName();
 
 (function() {
     var people = {
-        people: [],
+        people: ['Will', 'Steve'],
         init: function() {
             this.cacheDom();
         },
@@ -37,6 +50,10 @@ myModule.sayName();
             // https://www.youtube.com/watch?v=m-NYyst_tiY&list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f&index=2
             // THIS FEELS HELPFUL??
             // IDK BUT TIME TO WORKOUT
+            var data = {
+                people: this.people,
+            }
+            this.$ul.html(Mustache.render(this.template, data));
         }
     }
 
